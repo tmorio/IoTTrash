1173a6f6a794451df570816f555b727194686891
Takenori Morio <morikapusan@eleken.jp>
2019-01-20 00:33:06 +0900

    Update SigfoxArduino.ino

--- Device/SigfoxArduino.ino
+++ Device/SigfoxArduino.ino
@@ -1,4 +1,3 @@
-
  //  Send sample SIGFOX messages with UnaBiz UnaShield V2S Arduino Shield.
 //  This sketch includes diagnostics functions in the UnaShield.
 //  For a simpler sample sketch, see examples/send-light-level.
@@ -23,11 +22,12 @@ static const bool echo = true;  //  Set to true if the SIGFOX library should dis
 static const Country country = COUNTRY_JP;  //  Set this to your country to configure the SIGFOX transmission frequencies.
 static UnaShieldV2S transceiver(country, useEmulator, device, echo);  //  Uncomment this for UnaBiz UnaShield V2S Dev Kit
 static String response;  //  Will store the downlink response from SIGFOX.
+
 int temp,hum;
 int distance;
-int successCount = 0;
-int trig = 14;
-int echoD = 15;
+//int successCount = 0;
+int trig = 2;
+int echoD = 3;
 
 
 void setup() 
@@ -47,12 +47,15 @@ void setup()
   //  Check whether the SIGFOX module is functioning.
   if (!transceiver.begin())stop(F("Unable to init SIGFOX module, may be missing"));  //  Will never return.
 
-  //  Send a raw 12-byte message payload to SIGFOX.  In the loop() function we will use the Message class, which sends structured messages.
-  //transceiver.sendMessage("0102030405060708090a0b0c ");
-
   //  Delay 10 seconds before sending next message.
-  Serial.println(F("Waiting 1 seconds..."));
-  delay(1000);
+  Serial.println("-----------------------------");
+  Serial.println("MyBox Client Ver1.1");
+  Serial.println("Developed by IoT oyama Team");
+  Serial.println("-----------------------------");
+  Serial.println("Client >> Hello MyBox Client!");
+  Serial.println("Client >> Starting System...");
+
+  delay(2000);
 }
 
   
@@ -63,53 +66,69 @@ void loop()
   
     hum = bme.readHumidity();
     temp = bme.readTemperature();
-    digitalWrite(trig,LOW);
-    delayMicroseconds(1);
-    // 超音波を出力
-    digitalWrite(trig,HIGH);
-    
-    delayMicroseconds(11);
-    // 超音波を出力終了
-    digitalWrite(trig,LOW);
-    // 出力した超音波が返って来る時間を計測
-    int t = pulseIn(echoD,HIGH);
-    // 計測した時間と音速から反射物までの距離を計算
-    distance = t*((331.5+(0.6*temp))*0.005);
-    Serial.println(t);
-    Serial.print(distance);
-    Serial.println(" cm");
 
-    word bitTemp = temp * 1.0;
-    word bitHum = hum * 1.0;
-    word bitDis = distance / 100;
+    if(hum > 100 || hum < 10 || temp > 100){
+      Serial.println("Client >> Hum or Temp Error. Waiting 30 sec.");
+      Serial.print("Error Data >> Temp:");
+      Serial.print(temp);
+      Serial.print(" Hum:");
+      Serial.println(hum);
+      delay(30000); 
+    }else{
+      digitalWrite(trig,LOW);
+      delayMicroseconds(1);
+      // 超音波を出力
+      digitalWrite(trig,HIGH);
+      
+      delayMicroseconds(11);
+      // 超音波を出力終了
+      digitalWrite(trig,LOW);
+      // 出力した超音波が返って来る時間を計測
+      int t = pulseIn(echoD,HIGH);
+      // 計測した時間と音速から反射物までの距離を計算
+      distance = t*((331.5+(0.6*temp))*0.005);
+  
+      //Serial.println(t);
+      Serial.print("Sensor >> Available:");
+      Serial.print(distance / 100);
+      Serial.println(" cm");
+      Serial.print("Sensor >> Temp:");
+      Serial.print(temp);
+      Serial.println(" °C");
+      Serial.print("Sensor >> Hum:");
+      Serial.print(hum);
+      Serial.println(" %");
+  
+      word bitTemp = temp * 1.0;
+      word bitHum = hum * 1.0;
+      word bitDis = distance / 100;
+  
+      char HexDataTemp[16] = "";
+      char HexDataHum[16] = "";
+      char HexDataDis[16] = "";
+      sprintf(HexDataTemp, "%04x", bitTemp);
+      sprintf(HexDataHum, "%04x", bitHum);
+      sprintf(HexDataDis, "%04x", bitDis);
+  
+      String StrTemp = HexDataTemp;
+      String StrHum = HexDataHum;
+      String StrDis = HexDataDis;
+  
+      
+      String msg = StrTemp + StrHum + StrDis;
 
-    char HexDataTemp[16] = "";
-    char HexDataHum[16] = "";
-    char HexDataDis[16] = "";
-    sprintf(HexDataTemp, "%04x", bitTemp);
-    sprintf(HexDataHum, "%04x", bitHum);
-    sprintf(HexDataDis, "%04x", bitDis);
+      Serial.print("SendData >> Data:");
+      Serial.println(msg);
 
-    String StrTemp = HexDataTemp;
-    String StrHum = HexDataHum;
-    String StrDis = HexDataDis;
-
-    /*
-    Serial.println(bitDis);
-    Serial.println(HexDataTemp);
-    Serial.println(HexDataHum);
-    Serial.println(HexDataDis);
-    */
-    
-    String msg = StrTemp + StrHum + StrDis;
-
-    Serial.println(msg);
-    
-    if(transceiver.sendMessage(msg))
-    {
-      successCount++;
-      }
-    Serial.println(successCount);
-    delay(900000); 
+      Serial.println("SIGFOX >> Sending data...");
+      
+      if(transceiver.sendMessage(msg))
+      {
+        Serial.println("SIGFOX >> Success!");
+        //successCount++;
+        }
+      //Serial.println(successCount);
+      delay(900000); 
+    }
 
 }
