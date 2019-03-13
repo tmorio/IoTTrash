4578ce6f7d082d8a04ade8db3cd735ba0616a169
tmorio <morikapusan@eleken.jp>
2019-03-13 21:52:38 +0900

    Add:Credit

--- web/credit.php
+++ web/credit.php
@@ -24,17 +24,49 @@ a {
 
 <h3>Credit</h3>
 <img class="logo-image" src="img/logo.png"><br>
-Ver 1.0.0 (Release 2019/3/9)<br>
+Ver 1.0.1 (Release 2019/3/12)<br>
 Released by IoT oyama Team.<br>
 <h5>Developer</h5>
 
 <a href="https://github.com/tmorio" target="_blank">
 <div class="cardProfile">
 	<div class="boxProfile">
-		<img src="https://avatars0.githubusercontent.com/u/20278135?s=460&v=4" class="iconBox">
+		<img src="img/developer/devTm.jpg" class="iconBox">
 		<div>
 			<p>Takenori Morio</p>
 		</div>
 	</div>
 </div>
 </a>
+<div class="cardProfile">
+        <div class="boxProfile">
+                <img src="img/developer/devSh.jpg" class="iconBox">
+                <div>
+                        <p>Ryusei Shimane</p>
+                </div>
+        </div>
+</div>
+<div class="cardProfile">
+        <div class="boxProfile">
+                <img src="img/developer/devNs.jpg" class="iconBox">
+                <div>
+                        <p>Yu Ohki</p>
+                </div>
+        </div>
+</div>
+<div class="cardProfile">
+        <div class="boxProfile">
+                <img src="img/developer/devKt.jpg" class="iconBox">
+                <div>
+                        <p>Kyosuke Tachioka</p>
+                </div>
+        </div>
+</div>
+<div class="cardProfile">
+        <div class="boxProfile">
+                <img src="img/default.jpg" class="iconBox">
+                <div>
+                        <p>ちりがみ</p>
+                </div>
+        </div>
+</div>
